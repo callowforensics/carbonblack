3f0b82caa00c25ad2941bf5bcd0fb015c5e088d1
callowforensics <acallow@btinternet.com>
2017-03-06 10:05:59 +0000

    Update send_skype_messages.py

--- carbonblack/alerts/send_skype_messages.py
+++ carbonblack/alerts/send_skype_messages.py
@@ -1,13 +1,6 @@
 import socket
 from Skype4Py import Skype
 
-__author__ = "Andrew Callow"
-__copyright__ = "Andrew Callow"
-__title__ = "send_skype_alerts.py"
-__license__ = "Proprietary"
-__version__ = "1.0"
-__email__ = "acallow@btinternet.com"
-__status__ = "Prototype"
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(('localhost', 9999))
