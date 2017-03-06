d3d7ad8981e9b22f7fc9b82ae17b55d684b58217
callowforensics <acallow@btinternet.com>
2017-03-06 10:03:50 +0000

    Update get_mft.py

--- carbonblack/go_live/get_mft.py
+++ carbonblack/go_live/get_mft.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         print(r"abcdefghijklmnopqrstuvwxyz https://123.456.789.1 c:\output hosts_to_get_mft_from.txt"
               r" c:\fget.exe c:\7zip_x86 c:\7zip_x64")
         sys.exit()
-        
+
     cb_api = sys.argv[1]
     cb_url = sys.argv[2]
     output_dir = sys.argv[3]
@@ -23,20 +23,20 @@ if __name__ == "__main__":
     fget_location = sys.argv[5]
     seven_zip_x86_path = sys.argv[6]
     seven_zip_x64_path = sys.argv[7]
-    
+
     with open(hosts, "r") as f:
         hosts_to_get_from = [line.strip() for line in f if line != "\n"]
-        
+
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
     cb_logger.logger(output_dir)
-    
+
     triage = CbGoLive(cb_api_key=cb_api, cb_server_url=cb_url, cb_output_path=output_dir)
     triage.get_sensor_details()
     online = triage.online_sensors
-    
-    for host_to_get in hosts_to_get_from:    
+
+    for host_to_get in hosts_to_get_from:
         for sensor in online:
             if sensor["computer_name"].lower() == host_to_get.lower():
                 sensor_id, host, os_version = sensor["id"], sensor["computer_name"],\
@@ -46,7 +46,7 @@ if __name__ == "__main__":
                     zip_path = seven_zip_x64_path
                 else:
                     zip_path = seven_zip_x86_path
-        
+
                 # Start the session.
                 triage.setup_go_live_session(sensor_id=sensor_id, host=host)
                 if triage.go_live_session_status:
@@ -56,39 +56,40 @@ if __name__ == "__main__":
                         for filename in files:
                             fullpath = os.path.join(path, filename)
                             files_to_put.append(fullpath)
-        
+
                     # upload the 7zip files.
                     for file in files_to_put:
                         fullpath, filename = file
                         triage.put_file(file_to_put=fullpath, working_directory="c:\windows\carbonblack")
-        
+
                     # Put FGET.exe
                     triage.put_file(file_to_put=fget_location, working_directory=r"c:\windows\carbonblack")
-        
-                    # Execute the commands to get the MFT.
-                    triage.execute_command(command="c:\windows\carbonblack\FGET.exe -extract c:\$MFT "
-                                                   "c:\windows\carbonblack\c_mft.bin")
-        
-                    # Add file to archive.
-                    triage.execute_command(command=r"c:\windows\system32\cmd.exe /c c:\windows\carbonblack\7za.exe "
-                                                   r"a -tzip mft.zip c_mft.bin")
+
+                    # Execute the dir command for each drive letter.
+                    for drive_letter in triage.host_drives_list:
+                        triage.execute_command(command=r"c:\windows\carbonblack\FGET.exe -extract {0}:\$MFT "
+                                                       r"c:\windows\carbonblack\{0}_mft.b"
+                                                       .format(drive_letter[0]))
+                        # Add the output to an archive.
+                        triage.execute_command(command=r"c:\windows\system32\cmd.exe /c c:\windows\carbonblack\7za.exe "
+                                                       r"a -tzip mft.zip {0}_mft.b".format(drive_letter[0]))
 
                     # Get the archive.
                     triage.get_file(file=r"c:\windows\carbonblack\mft.zip")
-        
+
                     # Delete the mft file.
                     triage.delete_file(file=r"c:\windows\carbonblack\c_mft.bin")
 
                     # Delete the fget file.
-                    triage.delete_file(file=r"c:\windows\carbonblack\fget.exe")       
-                       
+                    triage.delete_file(file=r"c:\windows\carbonblack\fget.exe")
+
                     # Delete the 7zip files.
                     for file in files_to_put:
                         fullpath, filename = file
                         triage.delete_file(file="c:\windows\carbonblack\{}".format(filename))
-        
+
                     # Delete the archive.
                     triage.delete_file(file=r"c:\windows\carbonblack\mft.zip")
-        
+
                     # Close the session.
                     triage.close_session()
